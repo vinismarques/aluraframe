fbc2cb121d4daf9deba6b25ee59e742579a13185
Vinicius Marques <vinicius_marques@live.com>
2019-03-05 17:05:12 -0300

    Refatoração do NegociacaoService

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,81 +1,40 @@
 class NegociacaoService {
 
+    constructor() {
+
+        this._http = new HttpService();
+    }
+
+
     obterNegociacaoSemana() {
 
         return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open("GET", "negociacoes/semana");
-
-            xhr.onreadystatechange = () => {
-
-                /*
-                Value	State	            Description
-                0	    UNSENT              Client has been created. open() not called yet.
-                1	    OPENED	            open() has been called.
-                2	    HEADERS_RECEIVED    send() has been called, and headers and status are available.
-                3	    LOADING	            Downloading; responseText holds partial data.
-                4	    DONE	            The operation is complete.
-                */
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    } else {
-
-                        console.log(xhr.responseText);
-                        reject("Não foi possível obter as negociações da semana.");
-                    }
-                }
-            };
-
-            xhr.send();
-
+            this._http
+                .get("negociacoes/semana")
+                .then( negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(error => {
+                    console.log(erro);
+                    reject("Não foi possível obter as negociações da semana.");
+                });
         });
-
     }
 
     obterNegociacaoSemanaAnterior() {
 
         return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open("GET", "negociacoes/anterior");
-
-            xhr.onreadystatechange = () => {
-
-                /*
-                Value	State	            Description
-                0	    UNSENT              Client has been created. open() not called yet.
-                1	    OPENED	            open() has been called.
-                2	    HEADERS_RECEIVED    send() has been called, and headers and status are available.
-                3	    LOADING	            Downloading; responseText holds partial data.
-                4	    DONE	            The operation is complete.
-                */
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    } else {
-
-                        console.log(xhr.responseText);
-                        reject("Não foi possível obter as negociações da semana anterior.");
-                    }
-                }
-            };
-
-            xhr.send();
-
+            this._http
+                .get("negociacoes/anterior")
+                .then( negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(error => {
+                    console.log(erro);
+                    reject("Não foi possível obter as negociações da semana anterior.");
+                });
         });
 
     }
@@ -84,38 +43,15 @@ class NegociacaoService {
 
         return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open("GET", "negociacoes/retrasada");
-
-            xhr.onreadystatechange = () => {
-
-                /*
-                Value	State	            Description
-                0	    UNSENT              Client has been created. open() not called yet.
-                1	    OPENED	            open() has been called.
-                2	    HEADERS_RECEIVED    send() has been called, and headers and status are available.
-                3	    LOADING	            Downloading; responseText holds partial data.
-                4	    DONE	            The operation is complete.
-                */
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText)
-                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-
-                    } else {
-
-                        console.log(xhr.responseText);
-                        reject("Não foi possível obter as negociações da semana retrasada.");
-                    }
-                }
-            };
-
-            xhr.send();
-
+            this._http
+                .get("negociacoes/retrasada")
+                .then( negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(error => {
+                    console.log(erro);
+                    reject("Não foi possível obter as negociações da semana retrasada.");
+                });
         });
 
     }
