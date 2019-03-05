ba0dd6742cdb9c400eecc6aa15d17e4c551d3267
Vinicius Marques <vinicius_marques@live.com>
2019-03-05 17:02:56 -0300

    Criação do service HttpRequest

---
+++ client/js/app/services/HttpService.js
@@ -0,0 +1,39 @@
+class HttpService {
+
+    get(url) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+
+            xhr.open("GET", url);
+
+            xhr.onreadystatechange = () => {
+
+                /*
+                Value	State	            Description
+                0	    UNSENT              Client has been created. open() not called yet.
+                1	    OPENED	            open() has been called.
+                2	    HEADERS_RECEIVED    send() has been called, and headers and status are available.
+                3	    LOADING	            Downloading; responseText holds partial data.
+                4	    DONE	            The operation is complete.
+                */
+
+                if (xhr.readyState == 4) {
+
+                    if (xhr.status == 200) {
+
+                        resolve(JSON.parse(xhr.responseText));
+
+                    } else {
+
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+
+            xhr.send();
+
+        });
+    }
+}
