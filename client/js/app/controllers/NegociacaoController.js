194f140299c0ad5223b57aaf7833556834487ea4
Vinicius Marques <vinicius_marques@live.com>
2019-03-05 13:32:51 -0300

    Adicionado função de importação utilizando promises

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -27,43 +27,21 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
-    importa() {
+    importaNegociacoes() {
 
-        let xhr = new XMLHttpRequest();
+        let service = new NegociacaoService();
 
-        xhr.open("GET", "negociacoes/semana");
-
-        xhr.onreadystatechange = () => {
-
-            /*
-            Value	State	            Description
-            0	    UNSENT              Client has been created. open() not called yet.
-            1	    OPENED	            open() has been called.
-            2	    HEADERS_RECEIVED    send() has been called, and headers and status are available.
-            3	    LOADING	            Downloading; responseText holds partial data.
-            4	    DONE	            The operation is complete.
-            */
-
-            if (xhr.readyState == 4) {
-
-                if (xhr.status == 200) {
-
-                    JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                        .forEach(negociacao => {
-                            this._listaNegociacoes.adiciona(negociacao)
-                        });
-                    console.log("Negociações importadas com sucesso.");
-
-                }else {
-
-                    console.log(xhr.responseText);
-                    console.log("Não foi possível obter as negociações do servidor.");
-                }
-            }
-        };
-        
-        xhr.send();
+        Promise.all([
+            service.obterNegociacaoSemana(),
+            service.obterNegociacaoSemanaAnterior(),
+            service.obterNegociacaoSemanaRetrasada()
+        ]).then(negociacoes => {
+            negociacoes
+                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociações importadas com sucesso";
+        })
+        .catch(error => this._mensagem.texto = error);
     }
 
     _criaNegociacao() {
